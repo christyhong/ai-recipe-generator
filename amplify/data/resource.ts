8e7dd192ffaaf6ae04003fa52aa5a78544f0b82b
Christine Hong <hongchristine07@gmail.com>
2025-06-13 14:56:17 -0400

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -4,6 +4,7 @@ export const auth = defineAuth({
   loginWith: {
     email: {
       verificationEmailStyle: "CODE",
+      
       verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
       verificationEmailBody: (createCode) =>
         `Use this code to confirm your account: ${createCode()}`,
